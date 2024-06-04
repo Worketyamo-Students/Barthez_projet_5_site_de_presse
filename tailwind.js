7e2e1ecec26269bb5168d2966d2803acaefbc1a6
Your Name <you@example.com>
2024-06-04 20:24:52 +0100

    Barthez-Kenwou-Projet5-siteDePresse

---
+++ tailwind.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                clifford: '#da373d',
+                'dark-300': '#000E01',
+                'dark-500': '#443434',
+                'green-500': '#79AC78',
+                'green-700': '#618264',
+            },
+
+            maxHeight: {
+                'block': '30rem',
+            },
+        }, 
+        
+    }, 
+
+    plugins: [],
+  }
