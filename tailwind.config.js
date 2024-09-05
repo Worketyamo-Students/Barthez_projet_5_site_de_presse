46de41ec922e798e20736ac610588d35fb17f6d1
01Barthez <kenwoubarthez@gmail.com>
2024-09-05 03:11:54 +0100

    configurations initiales de tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,33 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      theme: {
+        extend: {
+          colors: {
+            primary: "#79AC78",
+            primary2: "#618264",
+            light: "#fff",
+            dark: "#000E01"
+          },
+        
+          container: {
+            center: true,
+            padding: {
+              DEFAULT: "20px",
+              sm: "32px",
+              lg: "48px",
+              xl: "64px",
+            }
+          },
+    
+          fontFamily: {
+            poppins: ["Poppins", "sans-serif"],
+            ibm: ["IBM Plex Sans", "sans-serif"]
+          }
+        },
+      },    
+    },
   },
   plugins: [],
 }
