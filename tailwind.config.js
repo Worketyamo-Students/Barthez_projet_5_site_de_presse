c93da9cccff275adbcb9159c17afc5db0e76877a
01Barthez <kenwoubarthez@gmail.com>
2024-09-13 12:17:44 +0100

    ready to start newly

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./src/**/*.{htm,js}"
+  ],
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: '16px',
+        sm: '24px',
+        lg: '48px',
+        xl: '56px',
+        '2xl': '64px',
+      },
+    extend: {},
+  },
+  plugins: [],
+  }
+}
